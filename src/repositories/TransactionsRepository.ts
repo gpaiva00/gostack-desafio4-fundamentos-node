f48022d4ebbddebb6ede89bbe7bf7ec1deda907f
gabriel paiva <gabrielpaiva@MacBook-Pro-de-gabriel.local>
2020-04-21 20:40:59 -0300

    Implemented challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
 import Transaction from '../models/Transaction';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
 }
 
 class TransactionsRepository {
@@ -14,15 +21,46 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const initIncome = 0;
+    const initOutcome = 0;
+
+    const income = this.transactions.reduce((acc, curr) => {
+      if (curr.type === 'income') return acc + curr.value;
+      return acc;
+    }, initIncome);
+
+    const outcome = this.transactions.reduce((acc, curr) => {
+      if (curr.type === 'outcome') return acc + curr.value;
+      return acc;
+    }, initOutcome);
+
+    const total = income - outcome;
+
+    const balance: Balance = {
+      income,
+      total,
+      outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    if (type === 'outcome') {
+      const { total } = this.getBalance();
+
+      if (value > total) throw Error("You don't have enough cash");
+    }
+
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
